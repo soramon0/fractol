e14c939e3ecd5bbfb4f4ff91dde9420802d1a07a
soramon0 <23435312+soramon0@users.noreply.github.com>
2025-03-27 13:28:50 +0000

    fix: free mlx resources

--- main.c
+++ main.c
@@ -23,12 +23,10 @@ int	main(int argc, char *argv[])
 		exit_usage();
 	mlx_ptr = mlx_init();
 	if (mlx_ptr == NULL)
-		exit_msg(STDERR_FILENO, "could not init graphics\n");
+		exit_msg(STDERR_FILENO, "could not init mlx\n");
 	window = mlx_new_window(mlx_ptr, 400, 300, fractol_set_str(set));
 	if (window == NULL)
 		exit_msg(STDERR_FILENO, "could not create window\n");
-	while (1)
-	{
-	}
-	return (0);
+	return (mlx_destroy_window(mlx_ptr, window), mlx_destroy_display(mlx_ptr),
+		free(mlx_ptr), EXIT_SUCCESS);
 }
